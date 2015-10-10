3fde5b1a39d6cb4a11c1c16b2929973da6019b2c
Ian McCunn <ianmccunn@gmail.com>
2015-10-09 20:59:39 -0700

    button

--- app/js/index.js
+++ app/js/index.js
@@ -7,11 +7,15 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+  console.log('preload');
 }
 
 function create() {
+  console.log('create');
+  var button = new Button(game, 10, 10, function(event) { console.log('button pressed')});
 }
 
 function update() {
   console.log('update');
+
 }
